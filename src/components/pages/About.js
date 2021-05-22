73c7fb10de52045a4117de5c81cefcc310de836e
Solomon Adache <solomonadache@gmail.com>
2021-05-23 00:15:10 +0100

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,7 @@ import React from 'react'
   return (
     <React.Fragment>
       <h1>About</h1>
-      <p>This is the TodoList app v1.0.0. It is part
-        of a react Crash course
+      <p>This is the TodoList app v1.0.
       </p>
     </React.Fragment>
   )
