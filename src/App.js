c23a1ce5d109eeebb17915c141a7d67efccec238
Solomon Adache <solomonadache@gmail.com>
2021-05-28 12:44:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
   markComplete = (id) => {
     this.setState({todos: this.state.todos.map(todo  => {if(todo.id === id){
-        todo.completed = todo.completed
+        todo.completed = !todo.completed
       }
       return todo;
     }) });
